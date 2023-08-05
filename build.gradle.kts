5685fc73de3f0883a693fb3067169f2e2089dda4
Natan Vieira do Nascimento <itroken99@gmail.com>
2023-08-05 20:00:27 -0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+}
+
+group = "me.devnatan.yoki.ui"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+    implementation(compose.desktop.currentOs)
+    implementation("me.devnatan:yoki:0.1.1")
+    implementation("cafe.adriel.voyager:voyager-navigator:1.0.0-rc05")
+}
+
+compose.desktop {
+    application {
+        mainClass = "me.devnatan.yoki.ui.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "yoki-compose"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ src/main/kotlin/me/devnatan/yoki/ui/screens/ScreenB.kt
@@ -0,0 +1,23 @@
+package me.devnatan.yoki.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+
+class ScreenB : Screen {
+
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Column {
+            Text(text = "Tela B")
+            Button(onClick = { navigator.pop() }) {
+                Text(text = "Voltar pra tela A")
+            }
+        }
+    }
+}
